d2e2cf17675d0d2ead154f898992ac29331ec23a
Jonas Berlin <jonas.berlin@nitorcreations.com>
2019-09-09 10:25:42 +0300

    Allow font-coverage to be run from any directory.
    Using this you can do:
    
    $ /path/to/font-coverage.pl <font.ttf>
    
    or make a link to your $HOME/bin:
    
    $ ln -s /path/to/font-coverage.pl $HOME/bin/font-coverage

--- font-coverage.pl
+++ font-coverage.pl
@@ -21,6 +21,7 @@ use Font::TTF;
 use Font::TTF::Font;
 use Font::TTF::Ttc;
 use Getopt::Std;
+use Cwd qw(realpath);
 
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 
@@ -64,11 +65,13 @@ _EOT_
 
 sub usage { HELP_MESSAGE; exit(1); }
 
+my $baseDir = realpath($0) =~ s![^/]+$!!r;
+
 sub list_supported_uni_ver {
 	print "Supported Unicode versions on this system:\n\n";
-	foreach (<include/*>) {
-		s#^include/##;
-		print $_."\n" if ( -f "include/$_/FontCoverage.pm" );
+	foreach (<$baseDir/include/*>) {
+		s#^$baseDir/include/##;
+		print $_."\n" if ( -f "$baseDir/include/$_/FontCoverage.pm" );
 	}
 	exit (0);
 }
@@ -77,14 +80,14 @@ usage if ( $opts{'h'} || (! @ARGV) );
 list_supported_uni_ver if $opts{'l'};
 
 $req_uni_ver = $opts{'u'} // $default_uni_ver;
-if (! -d "include/$req_uni_ver") {
+if (! -d "$baseDir/include/$req_uni_ver") {
 	print STDERR "No data for Unicode version '$req_uni_ver'\n";
 	print STDERR "Use '$0 -l' to list supported versions\n";
 	exit (3);
 }
 
 # Include data for appropriate unicode version
-unshift @INC, "./include/$req_uni_ver";
+unshift @INC, "$baseDir/include/$req_uni_ver";
 require FontCoverage;
 
 # strict warning suppresion
